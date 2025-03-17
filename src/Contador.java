75ba7a8312d16d0a9097430ee5f85fc9bac69efb
torresdantas <torresdantas14@gmail.com>
2025-03-16 23:00:52 -0300

    update estudos feitos

--- src/Contador.java
+++ src/Contador.java
@@ -1,37 +1,19 @@
-import java.util.Scanner;
-
-class ParametrosInvalidosException extends Exception {
-    public ParametrosInvalidosException(String mensagem) {
-        super(mensagem);
-    }
-}
+import entidade.Contar;
 
 public class Contador {
     public static void main(String[] args) {
-        Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.nextInt();
-        terminal.close();
+        int numerox1 = 7;
+        int numerox2 = 1;
 
         try {
+            Contar conta = new Contar(numerox1, numerox2);
 
-            contar(parametroUm, parametroDois);
+            for(int i = numerox1; i <= numerox2; i++) {
+                System.out.println("Imprimindo o número: " + i);
+            }
 
-        }catch (ParametrosInvalidosException exception) {
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
-        }
-
-    }
-    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
-        if(parametroUm >= parametroDois){
-            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
-        }
-
-        int contagem = parametroDois - parametroUm;
-        for(int i = 1; i <= contagem; i++){
-            System.out.println("Imprindo o número: " + i);
+        } catch (RuntimeException e) {
+            System.out.println("Erro: " + e.getMessage());
         }
     }
 }
